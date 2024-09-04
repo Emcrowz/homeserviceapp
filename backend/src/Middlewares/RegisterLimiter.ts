f9eb100d76ee3664e239431016f50fbae1eff85c
Martynas Vru <martynas.vru@outlook.com>
2024-09-04 19:26:53 +0300

    Small changes

--- backend/src/Middlewares/RegisterLimiter.ts
+++ backend/src/Middlewares/RegisterLimiter.ts
@@ -1,6 +1,5 @@
 import rateLimit from "express-rate-limit";
 
-// Create a rate limiting middleware for /register endpoint
 export const RegisterLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
   max: 1, // Limit each IP to 1 registration per windowMs
@@ -8,6 +7,6 @@ export const RegisterLimiter = rateLimit({
     status: 429,
     message: "Too many accounts created from this IP, please try again after 24 hours.",
   },
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  standardHeaders: true, 
+  legacyHeaders: false, 
 });
